eacab96751ac13071ccf3e1bd68bde1ca8079600
raj2577 <112565295+raj2577@users.noreply.github.com>
2023-04-28 14:05:47 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,30 +3,17 @@ import axios from "axios";
 
 
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
-// const options = {
-//     method: 'GET',
-//     url: BASE_URL,
-//     params: {
-//       part: 'snippet',
-//       videoId: 'M7FIvfx5J10'
-//     },
-//     headers: {
-//       'content-type': 'application/octet-stream',
-//       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-//       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-//     }
-//   };
+
 
 const options = {
-    // method: 'GET',
-    // url: BASE_URL,
+    
     params: {
       
       maxResults: '50'
     },
     headers: {
-      // 'content-type': 'application/octet-stream',
-      'X-RapidAPI-Key': "d54fa40da4msh403db611adfecc4p14f1e8jsn7bce46997439",
+[
+      'X-RapidAPI-Key': {Rapid_Api_Key},
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
